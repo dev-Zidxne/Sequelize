c3c367a19b3a2d2e56293d25a310e47b9aaf16f3
Zidane Innis <zidaneinnis@gmail.com>
2023-04-26 21:42:21 -0500

    Resetting the cart & Fetching and outputting orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -126,9 +126,11 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
+  let fetchedCart;
   req.user
     .getCart()
     .then((cart) => {
+      fetchedCart = cart;
       return cart.getProducts();
     })
     .then((products) => {
@@ -146,6 +148,9 @@ exports.postOrder = (req, res, next) => {
           console.log(err);
         })
         .then((result) => {
+          return fetchedCart.setProducts(null);
+        })
+        .then((result) => {
           res.redirect("/orders");
         });
     })
@@ -153,15 +158,14 @@ exports.postOrder = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render("shop/orders", {
-    path: "/orders",
-    pageTitle: "Your Orders",
-  });
-};
-
-exports.getCheckout = (req, res, next) => {
-  res.render("shop/checkout", {
-    path: "/checkout",
-    pageTitle: "Checkout",
-  });
+  req.user
+    .getOrders({ include: ["products"] })
+    .then((orders) => {
+      res.render("shop/orders", {
+        path: "/orders",
+        pageTitle: "Your Orders",
+        orders: orders,
+      });
+    })
+    .catch((err) => console.log(err));
 };
